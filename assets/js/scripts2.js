d9f95dfaf2ce35ecf37d0f0f9b2ade7c50630ba8
David Piesse <piesse@gmail.com>
2016-12-07 11:53:27 +0000

    Init Commit

---
+++ assets/js/scripts2.js
@@ -0,0 +1,14 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('.coming-soon').backstretch([
+      "assets/img/backgrounds/4.jpg"
+    , "assets/img/backgrounds/6.jpg"
+    ], {duration: 3000, fade: 750});
+
+  
+});
+
